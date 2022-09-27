9ee740bac4e6f0f18151f728d0d9f6837f159a74
Elias Martin <5715@holbertonstudents.com>
2022-09-27 11:29:53 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stadio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{/* Este codigo imprimira un mensaje en pantalla */
-	puts("\"Programming is like building a multilingual puzzle");
-	getchar();
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
