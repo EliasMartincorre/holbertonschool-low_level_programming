b14ea90886e6d6e1c3f368bbc0f732d84ceaa77f
Elias Martin <5715@holbertonstudents.com>
2022-09-27 11:19:40 -0700

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stadio.h>
+int main(void)
+{/* Este codigo imprimira un mensaje en pantalla */
+	puts("\"Programming is like building a multilingual puzzle");
+	getchar();
+	return (0);
+}
