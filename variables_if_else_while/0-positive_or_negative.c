d78f8be95239f1ccf9d875ce7b80eb9ce2571e6f
Elias Martin <5715@holbertonstudents.com>
2022-09-28 10:45:02 -0700

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - valor positivo
  *
- * return: return valor positivo
+ * Return: return valor positivo
  */
 int main(void)
 {
@@ -19,7 +19,7 @@ if (n < 0)
 {printf("%d is negative\n", n);
 }
 if (n == 0)
-{printf("%d is ZERO\n", n);
+{printf("%d is zero\n", n);
 }
 return (0);
 }
