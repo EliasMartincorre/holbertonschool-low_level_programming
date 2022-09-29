0a987a0668cb8726e9409a69fbdf7f333bfabd6f
Elias Martin <5715@holbertonstudents.com>
2022-09-29 16:43:49 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Smile in the mirror .
+*
+*Return: return 0
+*/
+int main(void)
+{
+int a = '0';
+char b = 'a';
+for (a = '0'; a <= '9'; a++)
+putchar('a');
+for (b = 'a'; b <= 'f'; b++)
+putchar('b');
+putchar('\n');
+return (0);
+}
