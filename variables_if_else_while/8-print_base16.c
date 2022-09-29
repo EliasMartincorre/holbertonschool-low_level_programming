cbbdbc7cf8e8a2bf15e3d35b1c3aaa0f3870147b
Elias Martin <5715@holbertonstudents.com>
2022-09-29 16:46:17 -0700

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 int a = '0';
 char b = 'a';
 for (a = '0'; a <= '9'; a++)
-putchar('a');
+putchar(a);
 for (b = 'a'; b <= 'f'; b++)
-putchar('b');
+putchar(b);
 putchar('\n');
 return (0);
 }
