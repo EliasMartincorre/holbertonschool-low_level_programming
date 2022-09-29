2573326904948c0c2b754eaf25388ea02929a07e
Elias Martin <5715@holbertonstudents.com>
2022-09-29 15:55:31 -0700

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Smile in the mirror .
+*
+*Return: return 0
+*/
+int main(void)
+{
+char a = 'z';
+for (a = 'z' ; a <= 'z' ; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
+
