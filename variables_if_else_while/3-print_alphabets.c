f4775aebea1cce36e9bcbd9bea75cbc3251a3fb3
Elias Martin <5715@holbertonstudents.com>
2022-09-29 13:51:57 -0700

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*  main - Escribir el alfabeto en minuscula
+*
+*Return: return 0
+*/
+int main(void)
+{
+char a = 'a';
+char A = 'A';
+for (a = 'a' ; a <= 'z' ; a++)
+{putchar(a); }
+for (a = 'A' ; a <= 'Z' ; a++)
+putchar(A);
+putchar('\n');
+return (0);
+}
