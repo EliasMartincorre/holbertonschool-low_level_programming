3d7a0371bbb4e41d3c60a0df51be374901846d02
Elias Martin <5715@holbertonstudents.com>
2022-09-29 15:35:01 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Numeros base diez .
+*
+*Return: return 0
+*/
+int main(void)
+{
+int a = '0';
+for (a = '0'; a <= '9'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
