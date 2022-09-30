3e8cfe1b7d64eda61c08277642c1e0c8015497fd
Elias Martin <5715@holbertonstudents.com>
2022-09-29 17:05:31 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Numeros base diez .
+*
+*Return: return 0
+*/
+int main(void)
+{
+char a = '0';
+for (a = '0'; a <= '9'; a++)
+	putchar(a);
+if (a <= '9')
+{
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
