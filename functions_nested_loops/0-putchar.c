5f9387f317919e43c86959b95b904f3c886d8f02
Elias Martin <5715@holbertonstudents.com>
2022-09-30 06:43:36 -0700

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Imprime _putchart
+*
+*_putchar - imprime un caracter
+* Return: 0
+* .
+ */
+int main(void)
+{
+char [10] = "_putchar\n";
+int b;
+for (b = 0; b <= 10; b++)
+putchar(a[b]);
+return (0);
+}
