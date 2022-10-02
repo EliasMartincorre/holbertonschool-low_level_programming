a0ad7c45e61bfc79845f6f9d091e1e178d01d70a
Elias Martin <5715@holbertonstudents.com>
2022-10-02 16:08:04 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Print the alphabet
+*Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	char letras = 'a';
+	int repeat = '0';
+
+	for (repeat = '0'; repeat <= '9'; repeat++)
+			{
+	for (letras = 'a'; letras <= 'z'; letras++)
+	_putchar(letras); }
+	_putchar('\n');
+}
