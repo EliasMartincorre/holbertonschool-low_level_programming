be2a984d4467a94403e7b7f0972763e36ed81073
Elias Martin <5715@holbertonstudents.com>
2022-10-03 13:24:42 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * add - suma dos variables int.
+  *
+  *@a : valor de la variable 1
+  *@b : valor de la variable 2
+  * Return: (c)
+  */
+int add(int a, int  b)
+{
+return (a + b);
+}
