aaceb98917b1748e743c6788e8e9796a8301215d
Elias Martin <5715@holbertonstudents.com>
2022-10-03 13:06:40 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,18 +7,18 @@
 void times_table(void)
 {
 	int num, mul, prod;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 	for (mul = 0; mul <= 9; mul++)
 	{
 	prod = num * mul;
 
-	if((prod / 10) == 0)
+	if ((prod / 10) == 0)
 	{
 	if (mul == 0)
 	{
-		_putchar(prod +'0');
+		_putchar(prod + '0');
 	}
 	else
 	{
@@ -31,7 +31,7 @@ void times_table(void)
 	else
 	{
 		_putchar(',');
-		_putchar(' ');	
+		_putchar(' ');
 		_putchar((prod / 10) + '0');
 		_putchar((prod % 10) + '0');
 	}
