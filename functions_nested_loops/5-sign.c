14bcea8bb851956576aa8fb496bb2f263b1f0c7c
Elias Martin <5715@holbertonstudents.com>
2022-10-02 17:57:01 -0700

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - Imprimira el signo de la variable.
+ *
+ * @n: variable.
+ *
+ * Return: 1 si el signo es positivo
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == '0')
+	{
+	_putchar('0');
+	return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
