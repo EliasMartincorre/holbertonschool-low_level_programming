b403910213e16986369746b6b4a2fc41034a034c
Elias Martin <5715@holbertonstudents.com>
2022-10-03 07:42:38 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * times_table - tablas del 1 al 9
+  *
+  *
+ */
+void times_table(void)
+{
+	int  tabla, multiplicador, resultado;
+
+	resultado = (tabla * multiplicador);
+
+	for (tabla = 0; tabla <= 9; tabla++)
+	{
+	for (multiplicador = 0; multiplicador <= 9; multiplicador++)
+		{
+	if (multiplicador != 9)
+
+	_putchar(resultado);
+	_putchar(',');
+	_putchar(' ');
+	if else(multiplicador == 9)
+	_putchar(resultado);
+		}
+	_putchar('\n');
+	}
+}
