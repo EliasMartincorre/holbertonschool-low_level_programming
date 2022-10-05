feb80736024e07ffc80520cd1b7c6978d7c4483d
Elias Martin <5715@holbertonstudents.com>
2022-10-05 09:13:31 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - imprime numeros deo 0 al 9. + nueva linea
+*
+* @num: var
+*/
+void print_numbers(void)
+{
+	int num;
+
+for (num = 48; num <= 57; num++)
+	{
+	_putchar(num);
+	}
+	_putchar('\n');
+}
