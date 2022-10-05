860ecdc08d8e6c6edf9ca8f5ff98a7342b37cdfd
Elias Martin <5715@holbertonstudents.com>
2022-10-05 08:41:39 -0700

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * mul - multiplica dos variables integers.
+  *
+  * @a: variable 1.
+  *
+  * @b: variable 2.
+  *
+  * Return: o
+  */
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
+
+
