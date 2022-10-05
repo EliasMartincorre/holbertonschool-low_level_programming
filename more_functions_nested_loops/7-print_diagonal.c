ccbde023164a4d834424088b16ad58dd10c40777
Elias Martin <5715@holbertonstudents.com>
2022-10-05 15:29:06 -0700

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_diagonal - diagonal
+*
+* @n: variable
+*/
+void print_diagonal(int n)
+{
+	int baja, c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	if (n >= 0)
+	{
+		for (baja = 1; baja <= n; baja++)
+		{
+		for (c = 1; c <= baja; c++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+}
