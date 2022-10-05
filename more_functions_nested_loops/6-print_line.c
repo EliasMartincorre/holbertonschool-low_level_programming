19add057e4b27ee5c358f4ca73f3bae6de0613df
Elias Martin <5715@holbertonstudents.com>
2022-10-05 14:32:19 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_line - int
+*
+*
+*/
+void print_line(int n)
+{
+	int b;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+for (b = 0; b <= n; b++)
+{
+	_putchar('_');
+	
+}
+_putchar('\n');
+}
