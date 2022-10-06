adbac909dd1f573f56e6c944493b35234cb704b5
Elias Martin <5715@holbertonstudents.com>
2022-10-05 18:03:19 -0700

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - cuadrado
+ *
+ * @size: bbg
+ */
+void print_square(int size)
+{
+	int baja, rep;
+	
+	if (size <= 0)
+		_putchar('\n');
+	
+	for (baja = 1; baja <= size; baja++)
+	{
+	for (rep = 1; rep <= size; rep++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+	}
+}
