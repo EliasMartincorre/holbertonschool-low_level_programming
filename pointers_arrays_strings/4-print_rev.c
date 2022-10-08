78884a1606493ebf9a57ba40ed784ce1737f4d95
Elias Martin <5715@holbertonstudents.com>
2022-10-07 18:46:32 -0700

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_rev - in reverse
+  *
+  * @s: string
+  */
+void print_rev(char *s)
+{
+int rev = 0;
+
+	for (; s[rev] != '\0';)
+	{
+		rev++;
+	}
+	for (rev -= 1; rev >= 0; rev--)
+		_putchar(s[rev]);
+}
