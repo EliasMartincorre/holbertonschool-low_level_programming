644bb28e76b75c5b1a97b9b53b7da40c488d1057
Elias Martin <5715@holbertonstudents.com>
2022-10-10 09:24:10 -0700

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,8 +7,11 @@
 void puts2(char *str)
 {
 	int imprime = 0;
-	if (str[0] != 0)
-	_putchar(str[0]);
+	
+	if (str[imprime] != 0)
+	
+		_putchar(str[0]);
+	
 	for (; str[imprime] != '\0'; )
 	{
 		imprime++;
