ab0c1515f3a00d048370394b8f979a81272a6fd4
Elias Martin <5715@holbertonstudents.com>
2022-10-10 12:04:32 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * puts_half - imprime la segunda mitad del string.
+  *
+  * @str: string.
+  */
+void puts_half(char *str)
+{
+	int imprime = 0;
+	int mitad;
+
+	for (; str[imprime] != '\0';)
+	{
+		imprime++;
+	}
+		if ((imprime % 2) == 0)
+			{
+			mitad = (imprime / 2);
+			for (; str[mitad] != '\0'; mitad++)
+
+			_putchar(str[mitad]);
+			}
+		else
+			{
+				mitad = ((imprime -= 1) / 2);
+				for (; str[mitad] != '\0'; mitad++)
+					_putchar(str[mitad]);
+			}
+		_putchar('\n');
+}
