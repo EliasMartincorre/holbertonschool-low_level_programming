0c9c236b749304bf7604b6518352bfde90af7b70
Elias Martin <5715@holbertonstudents.com>
2022-10-10 13:44:18 -0700

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -6,19 +6,23 @@
   * @n: dimension del array.
   */
 void print_array(int *a, int n)
-{	
+{
 	int contador = 0;
 
 	for (; contador < n; contador++)
-	{ 
-		if (contador != (n - 1))
 		{
-		 printf("%d, ", a[contador]);
+	if (contador != (n - 1))
+		{
+	printf("%d, ", a[contador]);
 		}
-	
-	else 
-	{
-		printf("%d\n", a[contador]);
-	}
-	}
+
+	else if (contador == (n - 1))
+		{
+	printf("%d\n", a[contador]);
+		}
+	else if ((n - 1) < 0)
+		{
+	printf("  \n")
+		}
+		}
 }
