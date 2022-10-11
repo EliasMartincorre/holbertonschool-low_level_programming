50407eb0c6fdb4ef740a39d15090219ed3ab5187
Elias Martin <5715@holbertonstudents.com>
2022-10-11 07:25:33 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,11 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int c = 0;
-	int valor_total_s1 = 0;
-	int valor_total_s2 = 0;
-
-	for (; s1[i] != '\0'; )
+	for ( ; *s1 != '\0' && *s2 != '\0' && *s1 == *s2; )
 	{
-		i++;
-	valor_total_s1 += s1[i];
+		s1++;
+		s2++;
 	}
-	for ( ; s2[c] != '\0'; )
-	{
-		c++;
-		valor_total_s2 += s2[c];
-	}
-	return (valor_total_s1 - valor_total_s2);
+	return (*s1 - *s2);
 }
+
