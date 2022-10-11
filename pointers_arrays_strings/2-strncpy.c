c81a6f94285858d6fd210e468a17d755d5b18155
Elias Martin <5715@holbertonstudents.com>
2022-10-11 06:20:04 -0700

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,15 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-              int i;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-    dest[i] = src[i];
-    for ( ; i < n; i++)
-    dest[i] = '\0';
-
-    return dest;
-           
+	{
+	dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
