cc7b59a9031f5ddb765d6722dbc06f4d0b60e7c8
Elias Martin <5715@holbertonstudents.com>
2022-10-10 17:11:41 -0700

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,28 @@
 #include "main.h"
 /**
-*_strcat - concatena string
+*_strncat - concatena string
 *
 * @dest: archivo donde se concatena
 *
 * @src: archivo a concatenar
 *
+* @n: limite.
 * Return: archivo concatenado.
 */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, b;
 
-    i = 0;
-    b = 0;
+	i = 0;
+	b = 0;
 
-    for (; dest[i] != 0; i++)
-    {
-         b++;
-    }
-    for (i = 0; src[i] && i <= (n - 1); i++)
-    {
-         dest[b++] = src[i];
-    }
-    return (dest);
+	for (; dest[i] != 0; i++)
+	{
+		b++;
+	}
+	for (i = 0; src[i] && i <= (n - 1); i++)
+	{
+		dest[b++] = src[i];
+	}
+	return (dest);
 }
