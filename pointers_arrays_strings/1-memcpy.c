867518cada03183cbf2a8f3924b33f2580f1e452
Elias Martin <5715@holbertonstudents.com>
2022-10-12 09:24:43 -0700

    !

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
+  * _memcpy - copy first n bytes, de *src al buffer *dest
+  * @dest: puntero al primer buffer
+  * @src: posiciones a copiar
+  * @n: constante
+  * Return: Puntero a dest.
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
 char *dp = dest;
 const char *sp = src;
-for (;n != 0; n--)
+for (; n != 0; n--)
 *dp++ = *sp++;
-return dest;
+return (dest);
 }
-
