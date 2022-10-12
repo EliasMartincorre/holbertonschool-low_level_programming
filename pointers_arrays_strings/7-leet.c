93931bb5c0257a3484c6457e1a82ec54e7bce5d8
Elias Martin <5715@holbertonstudents.com>
2022-10-12 07:13:42 -0700

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,23 +1,25 @@
 #include"main.h"
 /**
+  * leet- codifica 1337.
   *
+  * @s1: string ingresado
   *
-  *
+  * Return: string codificado.
   */
-char *leet(char *s1)  
+char *leet(char *s1)
 {
 	int i = 0;
 	int f;
 
-char s2[10] ={"oleatOLAT"};
-char s3[10] ={"0134701347"};
-	
+char s2[10] = {"oleatOLAT"};
+char s3[10] = {"0134701347"};
+
 	for (; s1[i] != '\0'; i++)
 	{
 		for (f = 0 ; s2[f] != '\0'; f++)
 		{
 			if (s1[i] == s2[f])
-			{	
+			{
 				s1[i] = s3[f];
 			}
 		}
