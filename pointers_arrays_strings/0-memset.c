eb4b94f8e8360a1427834490c5cec68a592cbc94
Elias Martin <5715@holbertonstudents.com>
2022-10-13 07:31:10 -0700

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
-  * _memset -mkk
+  * _memset - funcion que copia n baytes en s , devuelve puntero a s
   * @n: constante
-  * @b: 
+  * @b: espacio de memoria a sumar
+  * @s: puntero a un espacio de memoria
   * Return: un puntero a la zona de memoria s.
   */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char* p=s;
+	char  *p = s;
+
 	for ( ; n > 0; n--)
 	*p++ = (unsigned char)b;
-	return s;
+	return (s);
 }
