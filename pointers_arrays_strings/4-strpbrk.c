886e6ed4ebe5a6916f92fca4a912a317240fcd69
Elias Martin <5715@holbertonstudents.com>
2022-10-13 13:57:46 -0700

    4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+* _strpbrk - primer byts coincidente.
+*
+* @s: puntero
+* @accept: puntero
+* Return: ubicacion
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int p;
+
+for (i = 0; accept[i] != '\0'; i++)
+{
+	for (p = 0; s[p] != '\0'; p++)
+	{
+	if (accept[i] == s[p])
+	{
+		return (s + p - 5);
+	}
+	}
+}
+return (0);
+}
