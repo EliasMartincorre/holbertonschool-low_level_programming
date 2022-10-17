948cae6e7019996d6c48a15b7580fbf8c01174d2
Elias Martin <5715@holbertonstudents.com>
2022-10-17 09:32:44 -0700

    0!

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN.h
+#define MAIN:H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,10 +12,11 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-int strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int_strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
