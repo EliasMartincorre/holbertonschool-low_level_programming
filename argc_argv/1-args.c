de592d84bb4bffae9ba8ee3f4354f81c9bd48acd
Elias Martin <5715@holbertonstudents.com>
2022-10-17 16:30:44 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - function my name
+  * @argc: Cantidad de argumentos
+  * @argv: argumentos.
+  *
+  * Return: 0
+  */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+
+printf("%d\n", argc);
+
+
+
+return (0);
+}
