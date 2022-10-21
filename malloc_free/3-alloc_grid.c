c82a307a8db97bf366d65c2d69a18b49dc2392b5
Elias Martin <5715@holbertonstudents.com>
2022-10-21 14:25:32 -0700

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -30,3 +30,18 @@ int **alloc_grid(int width, int height)
 	}
 	return (prueba);
 }
+/**
+ * free_grid- funcion que libera la memoria dinamica array[a][b].
+ * @grid: funcion que devuelve matriz 2d.
+ * @height: cantidad de punteros de la matriz.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+	free(grid[i]);
+	}
+	free(grid);
+}
