c76f1a1241b2ac98a1361aedc098ffc6749e0dda
Elias Martin <5715@holbertonstudents.com>
2022-10-24 09:07:34 -0700

    3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+  * array_range- diferencia entre maximum and minimum.
+  *	@min: value minimum inside of array.
+  * @max: value maximum inside array.
+  * Return: pointer to new array.
+  */
+int *array_range(int min, int max)
+{	int i = 0, a = 0;
+	int *ptr;
+
+if (min > max)
+{
+return (NULL);
+}
+ptr = malloc(((max - min) + 2) * sizeof(int));
+if (ptr == NULL)
+{
+return (NULL);
+}
+a = ((max - min) + 1);
+for (i = 0; i <= a; i++, min++)
+{
+	ptr[i] = min;
+}
+return (ptr);
+}
