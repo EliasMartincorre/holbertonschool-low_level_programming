1d81654b3e4142e7975311873c0f93123d033f7d
Elias Martin <5715@holbertonstudents.com>
2022-10-27 16:59:15 -0700

    5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include"dog.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+  * free_dog - free memory.
+  * @d: pointer to memory to will be free
+  */
+void free_dog(dog_t *d)
+{
+if (d)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
