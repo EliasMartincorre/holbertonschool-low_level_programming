da0fd60c6decaea22a3627c170402fe81c6a51af
Elias Martin <5715@holbertonstudents.com>
2022-10-28 09:26:15 -0700

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,14 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	 int i;
 
-if (array == NULL)
+if (array && cmp && size > 0)
 {
-	return (0);
-}
-if (size <= 0)
-{
-	return (-1);
-}
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
@@ -26,5 +20,6 @@ if (cmp(array[i]) != 0)
 	return (i);
 }
 }
-return (i);
+}
+return (-1);
 }
