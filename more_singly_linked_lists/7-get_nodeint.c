f3b8b93d4a422ed6c64950636299ee16c7a153bb
Elias Martin <5715@holbertonstudents.com>
2022-11-18 06:26:22 -0800

    index node

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+/**
+  * get_nodeint_at_index - print value n of index.
+  * @head: pointer firs element of the list.
+  * @index:index searching.
+  * Return: pointer to a node index.
+  *
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{ unsigned int i = 0;
+
+	for (; i < index; i++)
+	{
+		head = head->next;
+	}
+return (head);
+}
