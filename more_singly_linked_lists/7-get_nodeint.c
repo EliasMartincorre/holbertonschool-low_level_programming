08b351b9cb979b3e7930c7ec005996d0b3dbae54
Elias Martin <5715@holbertonstudents.com>
2022-11-19 15:32:34 -0800

    fixed =?

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -15,6 +15,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (; i < index; i++)
 	{
+		if (head == NULL || head->next == NULL)
+		{
+			return (NULL);
+		}
+
 		head = head->next;
 	}
 return (head);
