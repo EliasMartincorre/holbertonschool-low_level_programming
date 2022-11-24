1a7206e7236ea427b7d61bc1f6dba5583f840bad
Elias Martin <5715@holbertonstudents.com>
2022-11-24 11:37:53 -0800

    its ok?

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert node in specifik position.
+ * @head: double pointer to begginin of the list.
+ * @idx: index to insert new node.
+ * @n: data int of member n.
+ *
+ * Return: new_node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, size_t idx, int n)
+{
+	dlistint_t *temp = *head;
+	size_t node;
+
+dlistint_t *newnode = malloc(sizeof(dlistint_t));/**allocate memory*/
+
+	if (newnode == NULL)/** verification of succefuly allocate*/
+	{
+		return (NULL);
+	}
+	newnode->n = n; /** se asigna el valor ingresado al member n*/
+
+	if (idx == 0)
+	{
+		newnode->next = temp;/** index cero new node equal head*/
+		newnode->prev = NULL;
+		*head = newnode;
+
+		return (newnode);
+	}
+
+	for (node = 0; node < (idx - 1); node++)/** recorre el index*/
+	{
+		if (temp == NULL || temp->next == NULL)/** verifica index no out of list*/
+		{
+			return (NULL);
+		}
+
+		temp = temp->next;/** avanza al siguiente nodo*/
+	}
+
+	newnode->next = temp->next;/**nuevo nodo apunta al siguiente index */
+	newnode->prev = temp;
+	temp->next = newnode;/** el nodo anterior aputa al nodo insertado*/
+	return (newnode);
+}
+
