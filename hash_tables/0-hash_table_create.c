56b82d87bc8cff63085cb35789755e5a3cca01cd
Elias Martin <5715@holbertonstudents.com>
2022-11-27 16:56:01 -0800

    correct memory allocate

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include"hash_tables.h"
 /**
   * hash_table_create - create a hashtable.
-  * @size the size of the array.
+  * @size: the size of the array.
   * Return: new table.
   *
   */
@@ -9,13 +9,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int iterar;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (table == NULL)/**memory allocate for the new table*/
 	{
 		return (NULL);
 	}
-	
 	table->size = size;/**define size of the array*/
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_table_t) * size);
 	if (table == NULL)/**allocate memory for the array*/
 	{
 		return (NULL);
