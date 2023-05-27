50f3b919ca9d81791452a3ebdabbb411a1a7dd4b
EliasMartincorre <martincorrea792@gmail.com>
2023-05-27 11:46:20 -0300

    betty header

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include<stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array (const int *array, size_t size, size_t min);
+void print_ay(const int *array, size_t size, size_t min);
+void print_array(const int *array, size_t size);
 #endif
 
