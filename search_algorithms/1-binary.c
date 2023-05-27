fa2c42d55dabbf63d3919659df8d13c0e2590737
EliasMartincorre <martincorrea792@gmail.com>
2023-05-27 11:42:34 -0300

    binary-search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,72 +1,49 @@
-#include"search_algos.h"
+#include "search_algos.h"
+
 /**
-  * binary_search - binary search.
-  * @array: array location.
-  * @size: size of array.
-  * @value: valor buscado
-  * Return: index or -1.
-  */
+ * binary_search - Performs a binary search on a given array
+ *
+ * @array: The array to perform the search on
+ * @size: Size of the array we're searching
+ * @value: Value we're searching for
+ *
+ * Return: Index where value was found
+*/
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0, middle = size / 2;
-	int index = -1;
-
-	if (array == NULL)
-	{
-		return (index);
-	}
-
-print_array(array, size, i);
-	for ( ; i < size;)
-	{
-		if (array[middle] > value)
-		{
-		size = middle - 1;
-		middle = ((size + i) / 2);
-		print_array(array, size, i);
-		}
-		else if (array[middle] < value)
-		{
-			i = middle + 1;
-			middle = ((size + i) / 2);
-			print_array(array, size, i);
-		}
-		else if (array[middle] == value)
-		{
-			i++;
-			print_array(array, size, i);
-			index = middle;
-			break;
-		}
-	}
-		return (index);
-}
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- * @i: kkkk.
- */
-void print_array(const int *array, size_t size, size_t i)
+int superior = size - 1, low = 0, medio = 0, i = 0;
+if (!array)
 {
-	if (size == i)
-	{
-		i--;
-	}
+	return (-1);
+}
+while (array[low] <= array[superior])
+{
+	medio = (low + superior) / 2;
 	printf("Searching in array: ");
-while (array && i < size)
-{
-if (i < size - 1)
-{
-printf("%d, ", array[i]);
+	for (i = low; i <= superior; i++)
+		{
+		if (i < superior)
+		{
+			printf("%d, ", array[i]);
+		}
+		else
+		{
+			printf("%d", array[i]);
+		}
+	}
+	printf("\n");
+	if (value > array[medio])
+	{
+		low = medio + 1;
+	}
+	if (value < array[medio])
+	{
+		superior = medio - 1;
+	}
+	if (value == array[medio])
+	{
+		return (medio);
+	}
 }
-else
-{
-printf("%d", array[i]);
-}
-++i;
-}
-printf("\n");
+return (-1);
 }
